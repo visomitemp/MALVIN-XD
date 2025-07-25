1d8708abdd701e49948f7d8205be944b06bfddae
visomitemp <visomi6302@uppedia.com>
2025-07-25 16:26:27 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~1ylhSD6K#V1NHqtwY5u1LBZL86JgfPePta1zZsU1m86wK0gMfduE",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "923416397511", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
